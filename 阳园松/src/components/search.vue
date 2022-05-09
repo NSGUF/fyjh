a057f627b46c745e8dc607f1f918fdc1a3334ab2
2249348957 <2249348957@qq.com>
2022-05-09 20:02:28 +0800

    fypx训前作业

---
+++ 阳园松/src/components/search.vue
@@ -0,0 +1,52 @@
+<template>
+    <input v-model="keywords"
+           placeholder="请输入关键字" />
+    <button @click.stop="search">搜索</button>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType, Ref, ref, reactive, watch, computed } from "vue";
+
+export default defineComponent({
+    name: "pagination",
+    props: {
+        page: {
+            type: Number,
+            default: 1
+        },
+        total: {
+            type: Number,
+            default: 1
+        },
+        pageSize: {
+            type: Number,
+            default: 10
+        }
+    },
+    setup(props, { emit }) {
+        const keywords = ref('') as Ref<string>;;
+        
+        const search = function () {
+            emit('search', { value:keywords.value });
+        }
+        return {
+            keywords,
+            search
+        }
+    }
+});
+</script>
+
+<style scoped>
+.pagination {
+    display: flex;
+    padding: 0;
+    margin: 10px 0 0 0;
+}
+
+.pagination li {
+    list-style-type:none;
+    margin-right: 20px;
+    cursor: pointer;
+}
+</style>
---
+++ 阳园松/src/interface/index.ts
@@ -0,0 +1,9 @@
+/**
+ * @file 接口
+ */
+
+//排序传参接口类型
+export interface SortConfig {
+    sortType: string,
+    label: string
+}
